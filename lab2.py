5529255520d9ce411f21f3c8eab7fd1ae7247634
wasif691 <wasif31y@gmail.com>
2025-03-12 08:59:50 +0500

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,21 @@
+#count=0
+#while(count<3):
+#    count=count+1
+#    print("Hello Geek")
+
+#single statement while block
+
+count=0
+while(count==0):
+    print("Hello Geek")
+
+
+#Iterating over a list
+
+#print("List Iteration")
+#l=["geeks","for","geeks"]
+#for i in l:
+#
+# 
+#     print(i)
+
