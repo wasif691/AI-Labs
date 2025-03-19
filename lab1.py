63bddbd923f22050492e41894595c2bd9d895888
wasif691 <wasif31y@gmail.com>
2025-03-19 18:42:50 +0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,39 @@
+#print("Statement 1")
+#print("Statement 2")
+#print ("Statement 1");print("Statement 2")
+
+#x=1
+#if x>0:
+#    print("Hello World")
+
+#a=1452
+#type(a)
+#print(type(a))
+#b=-4587
+#type(b)
+#print(type(b))
+#c=0
+#type(c)
+#print (type(c))
+#g=1.03
+#type(g)
+#print(type(g))
+#i=.34
+#type(i)
+#print(type(i))
+
+#my_list1=[5,12,13,14]
+#print(my_list1)
+
+#my_list2=['red','blue','black','white']
+#print(my_list2)
+
+#my_list3=['red',12,112.12]
+#print(my_list3)
+
+#color_list=["Red","Blue","Green","Black"]
+#color_list[0]
+#print(color_list[0],color_list[3])
+
+#print(color_list[-1])
+#print(color_list[3])
---
+++ lab2.py
@@ -0,0 +1,68 @@
+#count=0
+#while(count<3):
+#    count=count+1
+#    print("Hello Geek")
+
+#single statement while block
+
+#count=0
+#while(count==0):
+#    print("Hello Geek")
+
+
+#Iterating over a list
+
+#print("List Iteration")
+#l=["geeks","for","geeks"]
+#for i in l:
+#
+# 
+#     print(i)
+
+#print("\nTuple Iteration")
+#t=("geeks","for","geeks")
+#for i in t:
+#    print(i)
+
+# Python program to illustrate
+# Iterating by index
+#list = ["geeks", "for", "geeks"]
+#for index in range(len(list)):
+#    print (list[index])
+
+# Prints all letters except 'e' and 's'
+#for letter in 'geeksforgeeks':
+#    if letter == 'e' or letter == 's':
+#        continue
+#    print ('Current Letter :', letter)
+#    var = 10
+
+
+#for letter in 'geeksforgeeks':
+# break the loop as soon it sees 'e'
+# or 's'
+#    if letter == 'e' or letter == 's':
+#        break
+#    print ('Current Letter :'), letter   
+
+
+#def my_function():
+#    print("Hello from a function")
+#my_function()
+
+
+#def my_function(fname):
+#    print(fname + " Ref")
+#my_function("Email")
+#my_function("Tobias")
+#my_function("Linus")
+
+
+#def my_function(x):
+#    return 5 * x
+#print(my_function(3))
+#print(my_function(5))
+#print(my_function(9))
+
+class myclass: x=5
+p1=myclass() ;print(p1.x)
